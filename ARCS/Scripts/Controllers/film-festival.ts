f6730f6911ef28aea1ba4dde297b6d2544046881
Mike Hernandez <mihern@microsoft.com>
2017-11-11 17:10:00 -0800

    moving donor list to config file

--- ARCS/Scripts/Controllers/film-festival.ts
+++ ARCS/Scripts/Controllers/film-festival.ts
@@ -43,115 +43,18 @@ function FilmFestivalController($scope, $sce) {
         }
     ];
 
-    $scope.donorsARCS = [
-        "Roxana Arama",
-        "Alina Popa",
-        "Ana & Radu Bacioiu",
-        "Otilia & Tudor Baraboi",
-        "Gabriel & Raluca Hera",
-        "Ramona & Marian Pungan",
-        "Jeffrey Gossett",
-        "Dana Elena Cozmei",
-        "Elena & Cosmin Catrinescu",
-        "Irina & Cristi Salvan",
-        "Daniel & Claudia Pravăț",
-        "Alexandrina Măicăneanu & Radu Simionescu",
-        "Ileana Marin & Jim Augerot",
-        "Vasi Rusu (in memory of Eugenia Lup)",
-        "Pierre Loebel",
-        "Souren Aghajanyan"
-    ];
-
-    $scope.donorsA = [
-        "Mary Sherhart",
-        "Alexandra Dorca & Richard Pavy",
-        "Gabriel Ghizila",
-        "Carmen Rautu",
-        "Andrei Marinescu",
-        "Alexey Palladin",
-        "Ioana Miron",
-        "Anonymous"
-    ];
-
-    $scope.donorsR = [
-        "Radu Palanca",
-        "Stefan Saroiu",
-        "Elena & Adrian Fanaru",
-        "Ovidiu Constantin",
-        "Carl Winter",
-        "Elena Boris",
-        "Geanina Andreiu",
-        "Cezar Ungureanașu",
-        "Maria Gerea",
-        "Ancuța Zaharia",
-        "Verena Kuzmany",
-        "Ligia Cicos",
-        "Ana Ilac",
-        "Andrei Zlati",
-        "Nick Voicu",
-        "Cristina Teodorescu",
-        "Anca Ionescu",
-        "Ioana Pop",
-        "Yuko Mera",
-        "Anonymous (2)"
-    ];
-
-    $scope.donorsC = [
-        "Lisa Friedli",
-        "Smaranda Lieuallen",
-        "Seattle2Europe",
-        "Calin Popa"
-    ];
-
-    $scope.donorsS = [
-        "Maryna Ajaja",
-        "Linda Morris",
-        "Susan Picard",
-        "Alexander Price",
-        "Carol Arnold"
-    ];
-
-    $scope.partners = [
-        {
-            link: "http://www.icrny.org/",
-            imagePath: "rciny_logo_Kb.png"
-        },
-        {
-            link: "http://www.4culture.org/",
-            imagePath: "4culture.png"
-        },
-        {
-            link: "http://www.youragentnadia.com/",
-            imagePath: "Logo-Schmieder.jpg"
-        },
-        {
-            link: "http://www.traduterra.com/",
-            imagePath: "TraduTerra.png"
-        },
-        {
-            link: "https://cedarriverah.com/",
-            imagePath: "IMG_logo_Aluas.jpg"
-        }
-    ];
-
-    $scope.sponsors = [
-        {
-            link: "http://www.nwfamilydental.net/",
-            imagePath: "nwdental.png"
-        },
-        {
-            link: "http://www.drlarhs.com/",
-            imagePath: "ad-logo.jpg"
-        },
-        {
-            link: "http://www.mehlinglawfirm.com/",
-            imagePath: "logo2.jpg"
-        },
-        {
-            link: "https://www.redfin.com/",
-            imagePath: "Redfin LOGO.jpg"
-        }
-    ];
+    // let request = new XMLHttpRequest();
+    // request.open("GET", "./config.json");
+    // request.onload = function () {
+        $scope.donorsARCS = config["donorsARCS"];
+        $scope.donorsA = config["donorsA"];
+        $scope.donorsR = config["donorsR"];
+        $scope.donorsC = config["donorsC"];
+        $scope.donorsS = config["donorsS"];
+        $scope.partners = config["partners"];
+        $scope.sponsors = config["sponsors"];
+    // };
+    // request.send();
 
     $scope.guests = [
         {
---
+++ ARCS/Scripts/config.ts
@@ -0,0 +1,118 @@
+﻿let config = {
+    // Donors $500+
+    "donorsARCS": [
+        "Roxana Arama",
+        "Alina Popa",
+        "Ana & Radu Bacioiu",
+        "Otilia & Tudor Baraboi",
+        "Gabriel & Raluca Hera",
+        "Ramona & Marian Pungan",
+        "Jeffrey Gossett",
+        "Dana Elena Cozmei",
+        "Elena & Cosmin Catrinescu",
+        "Irina & Cristi Salvan",
+        "Daniel & Claudia Pravăț",
+        "Alexandrina Măicăneanu & Radu Simionescu",
+        "Ileana Marin & Jim Augerot",
+        "Vasi Rusu (in memory of Eugenia Lup)",
+        "Pierre Loebel",
+        "Souren Aghajanyan"
+    ],
+
+    // Donors $250+
+    "donorsA": [
+        "Mary Sherhart",
+        "Alexandra Dorca & Richard Pavy",
+        "Gabriel Ghizila",
+        "Carmen Rautu",
+        "Andrei Marinescu",
+        "Alexey Palladin",
+        "Ioana Miron",
+        "Anonymous"
+    ],
+
+    // Donors $100+
+    "donorsR": [
+        "Radu Palanca",
+        "Stefan Saroiu",
+        "Elena & Adrian Fanaru",
+        "Ovidiu Constantin",
+        "Carl Winter",
+        "Elena Boris",
+        "Geanina Andreiu",
+        "Cezar Ungureanașu",
+        "Maria Gerea",
+        "Ancuța Zaharia",
+        "Verena Kuzmany",
+        "Ligia Cicos",
+        "Ana Ilac",
+        "Andrei Zlati",
+        "Nick Voicu",
+        "Cristina Teodorescu",
+        "Anca Ionescu",
+        "Ioana Pop",
+        "Yuko Mera",
+        "Anonymous (2)"
+    ],
+
+    // Donors $50+
+    "donorsC": [
+        "Lisa Friedli",
+        "Smaranda Lieuallen",
+        "Seattle2Europe",
+        "Calin Popa"
+    ],
+
+    // Donors $25+
+    "donorsS": [
+        "Maryna Ajaja",
+        "Linda Morris",
+        "Susan Picard",
+        "Alexander Price",
+        "Carol Arnold"
+    ],
+
+    // Partners $1000+
+    "partners": [
+        {
+            "link": "http://www.icrny.org/",
+            "imagePath": "rciny_logo_Kb.png"
+        },
+        {
+            "link": "http://www.4culture.org/",
+            "imagePath": "4culture.png"
+        },
+        {
+            "link": "http://www.youragentnadia.com/",
+            "imagePath": "Logo-Schmieder.jpg"
+        },
+        {
+            "link": "http://www.traduterra.com/",
+            "imagePath": "TraduTerra.png"
+        },
+        {
+            "link": "https://cedarriverah.com/",
+            "imagePath": "IMG_logo_Aluas.jpg"
+        }
+    ],
+
+    // Sponsors $500+
+    "sponsors": [
+        {
+            "link": "http://www.nwfamilydental.net/",
+            "imagePath": "nwdental.png"
+        },
+        {
+            "link": "http://www.drlarhs.com/",
+            "imagePath": "ad-logo.jpg"
+        },
+        {
+            "link": "http://www.mehlinglawfirm.com/",
+            "imagePath": "logo2.jpg"
+        },
+        {
+            "link": "https://www.redfin.com/",
+            "imagePath": "Redfin LOGO.jpg"
+        }
+    ]
+}
