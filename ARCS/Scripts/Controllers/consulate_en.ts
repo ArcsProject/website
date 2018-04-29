2ca02c068377e0137ad7a112c8885314d7b5feb2
Claudiu Guiman <claudiu.guiman@gmail.com>
2018-04-29 13:45:39 -0700

    add consulate pages

---
+++ ARCS/Scripts/Controllers/consulate_en.ts
@@ -0,0 +1,7 @@
+﻿angular.module("arcsApp").component("consulateEn", {
+    templateUrl: "../../Views/consulate_en.html",
+    controller: ConsulateEnController
+});
+
+function ConsulateEnController($scope) {
+};
---
+++ ARCS/Scripts/Controllers/consulate_ro.ts
@@ -0,0 +1,8 @@
+﻿
+angular.module("arcsApp").component("consulateRo", {
+    templateUrl: "../../Views/consulate_ro.html",
+    controller: ConsulateRoController
+});
+
+function ConsulateRoController($scope) {
+};
