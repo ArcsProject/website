8900b0b15555308e6b37d074fcff0c2a7dc515da
Claudiu Guiman <claudiu.guiman@gmail.com>
2018-04-28 18:52:35 -0700

    update events

--- ARCS/Scripts/Controllers/events.ts
+++ ARCS/Scripts/Controllers/events.ts
@@ -14,6 +14,51 @@ function EventsController($scope, $stateParams) {
 
     $scope.events = [
         {
+            name: "Romanian Films at SIFF 2018, sponsored by ARCS",
+            description: ["Stay tuned for more details!"],
+            startDate: new Date(2018, 4, 3),
+            endDate: new Date(2018, 5, 10),
+            location: "SIFF Cinema Uptown",
+            locationLink: "https://www.google.com/maps/place/SIFF+Cinema+Uptown",
+            tags: ["community", "film", "stage"],
+            imagePath: "siff_content/siff2018.png",
+            moreInfoPath: "film-festival-2017"
+        },
+        {
+            name: "Club Piticot",
+            description: [
+                "Free",
+                "Kids ages 3-13",
+                "Let's get together to celebrate together International Children's Day! Event in Romanian."
+            ],
+            date: new Date(2018, 5, 2),
+            startTime: new Date(2018, 5, 2, 15, 0),
+            endTime: new Date(2017, 5, 2, 17, 0),
+            tags: ["community"],
+            imagePath: "Club-Piticot2.jpg",
+            moreInfoPath: "piticot"
+        },
+        {
+            name: "Inauguration of the Romanian Center of Excellence ROACT (Romanian In Action)",
+            description: ["Location to be determined"],
+            startDate: new Date(2018, 9),
+            endDate: new Date(2018, 9),
+            tags: ["community"],
+            imagePath: "excellence_center.jpg",
+            moreInfoPath: "excellence-center"
+        },
+        {
+            name: "Romanian Film Festival",
+            description: ["The fifth edition of the Romanian Film Festival"],
+            startDate: new Date(2018, 10, 2),
+            endDate: new Date(2018, 10, 4),
+            location: "SIFF Cinema Uptown",
+            locationLink: "https://www.google.com/maps/place/SIFF+Cinema+Uptown",
+            tags: ["community", "film", "stage"],
+            imagePath: "rffs_0_0.png",
+            moreInfoPath: "film-festival"
+        },
+        {
             name: "Special Exhibit: Romanian Identity, Royalty and Architecture",
             description: [
                 "Free",
@@ -76,17 +121,6 @@ function EventsController($scope, $stateParams) {
                     href: "http://www.press.uillinois.edu/books/catalog/65srs4wx9780252040764.html"
                 }
             ]
-        },
-        {
-            name: "Romanian Film Festival",
-            description: ["The fifth edition of the Romanian Film Festival"],
-            startDate: new Date(2018, 10, 2),
-            endDate: new Date(2018, 10, 4),
-            location: "SIFF Cinema Uptown",
-            locationLink: "https://www.google.com/maps/place/SIFF+Cinema+Uptown",
-            tags: ["community", "film", "stage"],
-            imagePath: "rffs_0_0.png",
-            moreInfoPath: "film-festival"
         },
         {
             name: "Ciprian Popescu - Meet the Author",
