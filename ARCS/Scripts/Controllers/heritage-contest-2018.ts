372230ad31fb7def2925eab2eafa12172449d324
Claudiu Guiman <claudiu.guiman@gmail.com>
2018-05-05 17:43:30 -0700

    Add heritage award pages

---
+++ ARCS/Scripts/Controllers/heritage-contest-2018.ts
@@ -0,0 +1,7 @@
+﻿angular.module("arcsApp").component("heritageContest2018", {
+    templateUrl: "../../Views/heritage-contest-2018.html",
+    controller: HeritageContest2018
+});
+
+function HeritageContest2018($scope) {
+};
