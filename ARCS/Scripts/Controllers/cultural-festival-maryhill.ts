bc69a50202b1c44088c39d6d7c8976352d844f28
Claudiu Guiman <claudiu.guiman@gmail.com>
2018-07-12 19:54:07 -0700

    add maryhill event

---
+++ ARCS/Scripts/Controllers/cultural-festival-maryhill.ts
@@ -0,0 +1,7 @@
+﻿angular.module("arcsApp").component("culturalFestivalMaryhill", {
+    templateUrl: "../../Views/cultural-festival-maryhill.html",
+    controller: CulturalFestivalMaryhill
+});
+
+function CulturalFestivalMaryhill($scope) {
+};
