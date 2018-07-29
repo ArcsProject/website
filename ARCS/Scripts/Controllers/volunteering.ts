d430dc6aac4ab9c8d48409c78b7338717001229a
Daniel Pravat <dpravat@msn.com>
2018-07-28 23:53:36 -0700

    Add volunteers.

---
+++ ARCS/Scripts/Controllers/volunteering.ts
@@ -0,0 +1,14 @@
+﻿angular.module("arcsApp").component("volunteering", {
+    templateUrl: GetUrlWithVersion("../../Views/volunteering.html"),
+    controller: ['$scope', '$stateParams', '$sce', VolunteeringController]
+});
+
+function VolunteeringController($scope, $stateParams, $sce) {
+    $scope.param1 = $stateParams['contactId'];
+    if ($scope.param1) {
+        $scope.iframe = $sce.trustAsResourceUrl('https://arcsproject.secure.force.com/GW_Volunteers__PersonalSiteContactInfo?contactId=' + encodeURIComponent($stateParams['contactId']));
+    }
+    else {
+        $scope.iframe = $sce.trustAsResourceUrl('https://arcsproject.secure.force.com/GW_Volunteers__PersonalSiteJobCalendar');
+    }
+}
