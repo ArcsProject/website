c0b357b43d8387dea374e7c19c3785c4bbd5f1b3
Claudiu Guiman <claudiu.guiman@gmail.com>
2018-08-04 14:44:35 -0700

    update press page

--- ARCS/Scripts/Controllers/press.ts
+++ ARCS/Scripts/Controllers/press.ts
@@ -1,45 +1,13 @@
 ﻿angular.module("arcsApp").component("press", {
     templateUrl: GetUrlWithVersion("../../Views/press.html"),
-    controller: PressController
+    controller: ['$scope', '$sce', 'asyncContent', PressController]
 });
 
-function PressController($scope) {
-    $scope.pressImages = [
-        {
-            title: "Romanian Film Festival - Scarred Hearts",
-            src: "ScarredHearts2.jpg"
-        },
-        {
-            title: "Romanian Film Festival - Back Home",
-            src: "(c) Acasa la Tata, regia Andrei Cohn. Mandragora (4).jpg"
-        },
-        {
-            title: "Romanian Film Festival - Breaking News",
-            src: "BREAKING_NEWS-stills-003.jpg"
-        },
-        {
-            title: "Romanian Film Festival - This Baba",
-            src: "ThisBabaNoText.jpg"
-        },
-        {
-            title: "Romanian Film Festival - The New Gypsy Kings",
-            src: "CALIU & TARAF DE HAIDOUKS.jpg"
-        },
-        {
-            title: "Romanian Film Festival - Sieranevada",
-            src: "Sieranevada still 2.jpg"
-        },
-        {
-            title: "Romanian Film Festival - The Rest is Silence",
-            src: "Restul e tacere 5.jpg"
-        },
-        {
-            title: "Romanian Film Festival - 2 Lottery Tickets",
-            src: "doua lozuri 1.jpg"
+function PressController($scope, $sce, asyncContent) {
+    asyncContent.getContent("press", $sce, ['description']).then(function (content) {
+        for (var item in content) {
+            $scope[item] = content[item];
         }
-    ];
-
-    $scope.setSelectedImg = function (img) {
-        $scope.selectedImg = img;
-    };
+        $scope.DataLoaded = true;
+    });
 };
