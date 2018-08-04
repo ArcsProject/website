f6542fad9eb8e672220aa3554dde4e6b11c3635f
Claudiu Guiman <claudiu.guiman@gmail.com>
2018-08-03 18:48:32 -0700

    add "Romania Neimblanzita"

--- ARCS/Scripts/Controllers/film-festival.ts
+++ ARCS/Scripts/Controllers/film-festival.ts
@@ -21,5 +21,14 @@ function FilmFestivalController($scope, $sce) {
     ];
 
     $scope.films = [
+        {
+            name: "Untamed Romania",
+            description: [$sce.trustAsHtml("\"Untamed Romania\" explores \"one of the wildest and most beautiful parts of Europe\", from the mysterious Danube Delta to the unknown Carpathian forests. Wolves, bears, and lynx roam the vast forests where almost all the original flora and fauna still exist left untouched since time immemorial. There is no other location of comparable size with such pristine nature in Europe. This unique documentary is narrated by award-winning Romanian film and stage actor Victor Rebengiuc.")],
+            artist: "Tom Barton-Humphreys",
+            length: "90 minutes",
+            genres: "Documentary",
+            imagePath: "romania_neimblanzita1.jpg",
+            year: "2018"
+        },
     ];
 };
