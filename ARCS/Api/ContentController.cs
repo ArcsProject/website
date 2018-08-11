a4e4e6bce6e357fa701e498408c9322b43b12fc0
Claudiu Guiman <claudiu.guiman@gmail.com>
2018-08-11 12:50:31 -0700

    serve filmfest content from controller

---
+++ ARCS/Api/ContentController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ARCS.Api
+{
+    public class ContentController : ApiController
+    {
+        [Route("api/content/{target}")]
+        public async Task<object> GetContent(string target)
+        {
+            if (StaticContent.JsonContent.TryGetValue("content_" + target, out var content))
+            {
+                return content;
+            }
+            return await NotFound().ExecuteAsync(new CancellationToken());
+        }
+    }
+}
