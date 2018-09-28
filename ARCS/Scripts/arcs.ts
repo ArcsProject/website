e8f7c1aa9c8ede52e7846a6d052af3fb699a8ab9
Claudiu Guiman <claudiu.guiman@gmail.com>
2018-09-28 15:42:38 -0700

    Revert "remove beacon transport"
    This reverts commit f9a37c708fd762e01bb85e5458ed92a578643e7c.

--- ARCS/Scripts/arcs.ts
+++ ARCS/Scripts/arcs.ts
@@ -28,6 +28,7 @@ let TrackOutboundRequests = function () {
             ga('send', 'event', {
                 eventCategory: 'Outbound Link',
                 eventAction: evntAction,
+                transport: 'beacon',
                 eventLabel: evntLabel,
                 eventValue: evntValue
             });
